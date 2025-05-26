764ff59b2a3d4d74a3f6ee85f68413f9caae3783
Abhigyan Raj <abhigyanraj@ABHIGYAN.local>
2025-05-26 11:06:36 +0530

    Update AuthPage.jsx - abhigyan

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -143,7 +143,7 @@ function AuthPage() {
                   autoComplete="tel"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
-                  placeholder="Phone Number (e.g., +11234567890)"
+                  placeholder="Phone Number (e.g., +91 934567890)"
                   value={phoneNumber}
                   onChange={(e) => setPhoneNumber(e.target.value)}
                   disabled={!!verificationId}
